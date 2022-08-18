d3a0992bffde54ec14df4eddd9630eaab8b646dd
salvagedscrap <kivrakdalmustafacagri@gmail.com>
2022-08-18 21:52:53 +0300

    new challenge

---
+++ isHeGonnaSurvive.js
@@ -0,0 +1,14 @@
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+function hero(bullets, dragons){
+    //Get Coding!
+      if(bullets >= dragons * 2){
+        return true
+      }else{
+        return false
+      }
+    }
