5e7120b05a4c3275ca80848e1ef72d16a0cef74f
salvagedscrap <kivrakdalmustafacagri@gmail.com>
2022-08-18 20:39:00 +0300

    new challenge

---
+++ friendOrFoe.js
@@ -0,0 +1,14 @@
+// Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+// Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+function friend(friends){
+    //your code here
+    
+     let newArr = friends.filter(e=>e.length===4)
+     return newArr
+  }
+  //function creates new array that includes element filtered with e.length to check names if they have exactly 4 letters
+  
