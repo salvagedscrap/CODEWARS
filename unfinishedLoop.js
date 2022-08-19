17eff44ddcb0ba743d57cc6bccc76cbcc822fff4
Mustafa Cagri Kivrakdal <kivrakdalmustafacagri@gmail.com>
2022-08-19 21:44:13 +0300

    new challenge

---
+++ unfinishedLoop.js
@@ -0,0 +1,11 @@
+// Unfinished Loop - Bug Fixing #1
+
+function createArray(number){
+    var newArray = [];
+    
+    for(var counter = 1; counter <= number;){
+      newArray.push(counter);
+    }
+    
+    return newArray;
+  }
