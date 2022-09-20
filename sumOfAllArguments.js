03f8d2db0262f0b1cfd98afe296ff51dc4e4ff6b
Mustafa Cagri Kivrakdal <kivrakdalmustafacagri@gmail.com>
2022-09-20 19:46:34 +0300

    new challenge

---
+++ sumOfAllArguments.js
@@ -0,0 +1,19 @@
+// Description:
+
+// Write a function that finds the sum of all its arguments.
+
+// eg:
+
+// sum(1, 2, 3) // => 6
+// sum(8, 2) // => 10
+// sum(1, 2, 3, 4, 5) // => 15
+
+
+
+function sum() {
+    let total = 0
+    for(let i = 0; i<arguments.length; i++){
+      total += arguments[i]
+    }
+    return total
+  }
