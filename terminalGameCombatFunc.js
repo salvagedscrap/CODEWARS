91f4fddde6d0546ac3e94f97308c554ba823b28a
Mustafa Cagri Kivrakdal <kivrakdalmustafacagri@gmail.com>
2022-10-15 09:15:05 +0300

    new challenge

---
+++ terminalGameCombatFunc.js
@@ -0,0 +1,10 @@
+// Description:
+
+// Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0.
+
+function combat(health, damage) {
+    if(health - damage<0 ){
+      return 0
+    }
+    return health - damage
+  }
