4da52b4a399c5380ca2c8d15851fc8c1d328c9ce
Mustafa Cagri Kivrakdal <kivrakdalmustafacagri@gmail.com>
2022-10-17 09:13:38 +0300

    new challenge

---
+++ helloNameOrWorld.js
@@ -0,0 +1,26 @@
+// Description:
+
+// Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! if name is not given (or passed as an empty String).
+
+// Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx).
+
+// Examples:
+
+// * With `name` = "john"  => return "Hello, John!"
+// * With `name` = "aliCE" => return "Hello, Alice!"
+// * With `name` not given 
+//   or `name` = ""        => return "Hello, World!"
+
+
+
+function hello(name) {
+  
+    if(name){
+  const result = name.charAt(0).toUpperCase() +
+                 name.slice(1).toLowerCase();
+      return `Hello, ${result}!`
+    }else{
+      return 'Hello, World!'
+    }
+    
+  }
